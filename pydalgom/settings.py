1445650c1852caac79fd28c849569926f6c79316
Cesar Gutierrez <cesar@tineo.mobi>
2018-10-19 17:25:37 -0500

    fix camelcases from database

--- pydalgom/settings.py
+++ pydalgom/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
-del DATABASES['default']['OPTIONS']['sslmode']
+if 'sslmode' in DATABASES['default']['OPTIONS']:
+    del DATABASES['default']['OPTIONS']['sslmode']
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
